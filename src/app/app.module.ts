2af35551c3144152910a0a675197c35c1da1bec4
Jhon Suarez <jhon.suarez@ceiba.com.co>
2023-01-17 08:36:54 -0500

    Creacion de tabla basica en handsontable con encapsulamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HotTableModule } from '@handsontable/angular';
+import { registerAllModules } from 'handsontable/registry';
+
 import { AppComponent } from './app.component';
 
+// register Handsontable's modules
+registerAllModules();
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HotTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
